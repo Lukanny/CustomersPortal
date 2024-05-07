8f9382eefd7aaa4fbf7fc26480733ab0c851b7da
Vinicius <vinicius_loureiro_cavalcante@hotmail.com>
2024-05-07 17:29:02 -0400

    Defininf files and customers models.

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Customer(models.Model):
+    company_worker_name = models.CharField(max_length=254)
+    company_worker_id = models.CharField(max_length=9)
+    company_worker_nif = models.CharField(max_length=11)
+    company_worker_position = models.CharField(max_length=254)
+    company_name = models.CharField(max_length=254)
+    company_address = models.CharField(max_length=254)
+    company_number = models.CharField(max_length=15)
+    company_email = models.EmailField(max_length=254)
+    company_registered_at = models.DateTimeField(editable=False)
+    company_last_edition = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.company_registered_at = timezone.now()
+        self.company_last_edition = timezone.now()
+        return super(Customer, self).save(*args, **kwargs)
+    
+    def __str__(self):
+        return f"{self.company_name}"
+    
+
--- files/models.py
+++ files/models.py
@@ -1,3 +1,28 @@
+from customers.models import Customer
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+import os
+
+def file_upload_path(instance, filename):
+    customer_name = instance.customer.company_name
+    year = instance.file_uploaded_at.year
+    return os.path.join(filename, str(year), customer_name)
+
+
+class File(models.Model):
+    file_customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    file_name = models.CharField(max_length=255)
+    file_description = models.TextField(blank=False, null=False)
+    file_path = models.FileField(upload_to=file_upload_path)
+    file_uploaded_at = models.DateTimeField(editable=False)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.company_registered_at = timezone.now()
+        self.company_last_edition = timezone.now()
+        return super(Customer, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.file_name}"
+    
