294490c2e13f68ff419fa3979af55324d6671b3e
Vinicius Loureiro Cavalcante <vinicius.loureiro.cavalcante12@gmail.com>
2024-12-31 16:26:11 -0400

    adding static files to root dir isntead of app.

--- core/settings.py
+++ core/settings.py
@@ -144,9 +144,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT = '/app/static'
+STATIC_ROOT = '/static'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'core/static')]
 
 
 # Default primary key field type
