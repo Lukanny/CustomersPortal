4a1be886aeffd309335a91ec7759eb922a989c45
Vinicius Loureiro Cavalcante <vinicius.loureiro.cavalcante12@gmail.com>
2025-01-07 19:14:41 -0400

    ajudsting nginx conf file and new django user permissions.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Ensure MySQL is ready
 until python -c "import MySQLdb; MySQLdb.connect(host='${DATABASE_HOST}', port=int('${DATABASE_PORT}'), user='${MYSQL_USER}', passwd='${MYSQL_PASSWORD}', db='${MYSQL_DATABASE}')"; do
     echo "Waiting for MySQL to be ready..."
     sleep 1
@@ -7,12 +8,20 @@ done
 
 echo "Database is ready!"
 
+# Adjust ownership of static files
+echo "Adjusting static files permissions..."
+chown -R django_user:django_user /app/static
+chmod -R 755 /app/static
+
+# Collect static files
 echo "Collecting static files..."
 python manage.py collectstatic --noinput --verbosity 2
 
+# Apply migrations
 echo "Applying database migrations..."
 python manage.py migrate --noinput
 
+# Start Gunicorn
 exec gunicorn core.wsgi:application \
   --workers=2 \
   --threads=2 \
