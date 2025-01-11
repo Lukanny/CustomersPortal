e900ec84e1dd7de89512788122da4d780f4a2065
Vinicius Loureiro Cavalcante <vinicius.loureiro.cavalcante12@gmail.com>
2025-01-10 21:49:39 -0400

    removing duplicate admin register of empresa model.

--- customers/admin.py
+++ customers/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from .models import Empresa, Representante
 
-admin.site.register(Empresa)
+admin.site.register(Representante)
 
 @admin.register(Empresa)
 class EmpresaAdmin(admin.ModelAdmin):
