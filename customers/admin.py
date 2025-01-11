6ebe4225f77f955cc18f9e47b37f1658f8f6cc66
Vinicius Loureiro Cavalcante <vinicius.loureiro.cavalcante12@gmail.com>
2025-01-11 09:12:05 -0400

    changing empresa and arquivo views on admin panel.

--- customers/admin.py
+++ customers/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
+from .models import Empresa
+from files.models import Arquivo
 
-from .models import Empresa, Representante
-
-admin.site.register(Representante)
+class ArquivoInline(admin.TabularInline):
+    model = Arquivo
+    extra = 0
+    fields = ('nome_do_arquivo', 'ano_do_arquivo', 'endereço_do_arquivo')
+    readonly_fields = ('ano_do_arquivo', 'endereço_do_arquivo')
 
 @admin.register(Empresa)
 class EmpresaAdmin(admin.ModelAdmin):
-    search_fields = ['nome_fantasia_da_empresa'] 
-    list_display = ('nome_fantasia_da_empresa', 'endereço_da_empresa', 'número_de_telefone_da_empresa', 'cnpj_da_empresa') 
-    list_filter = ('data_de_registro_da_empresa',)
-    ordering = ('nome_fantasia_da_empresa',)
+    list_display = ('nome_fantasia_da_empresa', 'cnpj_da_empresa', 'número_de_telefone_da_empresa')
+    search_fields = ('nome_fantasia_da_empresa', 'cnpj_da_empresa')
+    inlines = [ArquivoInline]
+    
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.prefetch_related('files')  # Carrega arquivos relacionados para performance
--- files/admin.py
+++ files/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-
 from .models import Arquivo
 
-admin.site.register(Arquivo)
+@admin.register(Arquivo)
+class ArquivoAdmin(admin.ModelAdmin):
+    list_display = ('cliente', 'nome_do_arquivo', 'ano_do_arquivo', 'data_de_upload_do_arquivo')
+    list_filter = ('ano_do_arquivo',)
+    search_fields = ('nome_do_arquivo', 'cliente__nome_fantasia_da_empresa')
+    ordering = ('cliente', 'ano_do_arquivo', 'nome_do_arquivo')
 
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.select_related('cliente')  # Melhora a performance carregando o cliente junto
