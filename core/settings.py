1d8ba473aa63fe88da9ddd7be7dcba094b7cd296
Vinicius Loureiro Cavalcante <vinicius.loureiro.cavalcante12@gmail.com>
2025-01-25 18:54:16 -0400

    adding new allowed host.

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     'prosesmtsegurancadotrabalho.com',
     'www.prosesmtsegurancadotrabalho.com',
+    '18.118.95.8',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
