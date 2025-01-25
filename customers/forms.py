b91787838a24fd390966172b88b0c158ab91b990
Vinicius Loureiro Cavalcante <vinicius.loureiro.cavalcante12@gmail.com>
2025-01-25 14:05:34 -0400

    adding Nome da Empresa to forms, changing color of fields that are readonly.

--- customers/forms.py
+++ customers/forms.py
@@ -4,34 +4,63 @@ from .models import Representante
 
 class RepresentanteForm(forms.ModelForm):
     cnpj_da_empresa = forms.CharField(
-        max_length=12, 
-        required=False, 
+        max_length=12,
+        required=False,
         widget=forms.TextInput(attrs={
-            'class': 'form-control form-control-md bg-light fs-6 text-dark',
+            'class': 'form-control form-control-md fs-6 text-light',
             'readonly': 'readonly',
+            'style': 'background-color: #343a40;',
         })
     )
+    nome_fantasia_da_empresa = forms.CharField(
+        max_length=254,
+        required=False,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control form-control-md fs-6 text-light',
+            'readonly': 'readonly',
+            'style': 'background-color: #343a40;',
+        }),
+        label="Nome da Empresa"
+    )
+
     class Meta:
         model = Representante
-        fields = ['nome_do_representante_legal', 'cpf_do_representante_legal', 'cargo_do_representante_legal', 'email_do_representante_legal', 'cnpj_da_empresa']
-    
+        fields = [
+            'nome_do_representante_legal',
+            'cpf_do_representante_legal',
+            'cargo_do_representante_legal',
+            'email_do_representante_legal',
+            'cnpj_da_empresa',
+            'nome_fantasia_da_empresa'
+        ]
+
     def __init__(self, *args, **kwargs) -> None:
         super(RepresentanteForm, self).__init__(*args, **kwargs)
-        self.fields['nome_do_representante_legal'].widget.attrs.update({'class': 'form-control form-control-md bg-light fs-6 text-dark'})
-        self.fields['nome_do_representante_legal'].widget.attrs['readonly'] = True
-        self.fields['nome_do_representante_legal'].label = "Nome"
 
+        readonly_fields = [
+            'nome_do_representante_legal',
+            'cpf_do_representante_legal',
+            'cnpj_da_empresa',
+            'nome_fantasia_da_empresa'
+        ]
 
-        self.fields['cpf_do_representante_legal'].widget.attrs.update({'class': 'form-control form-control-md bg-light fs-6 text-dark'})
-        self.fields['cpf_do_representante_legal'].widget.attrs['readonly'] = True
-        self.fields['cpf_do_representante_legal'].label = "CPF"
+        for field in readonly_fields:
+            self.fields[field].widget.attrs.update({
+                'readonly': True,
+                'class': 'form-control form-control-md fs-6 text-light',
+                'style': 'background-color: #343a40;'
+            })
 
-        self.fields['cargo_do_representante_legal'].widget.attrs.update({'class': 'form-control form-control-md bg-light fs-6 text-dark'})
+        if self.instance and self.instance.empresa:
+            self.fields['cnpj_da_empresa'].initial = self.instance.empresa.cnpj_da_empresa
+            self.fields['nome_fantasia_da_empresa'].initial = self.instance.empresa.nome_fantasia_da_empresa
+
+        self.fields['cargo_do_representante_legal'].widget.attrs.update({
+            'class': 'form-control form-control-md fs-6 text-dark bg-light',
+        })
         self.fields['cargo_do_representante_legal'].label = "Cargo Atual"
 
-        if self.instance and self.instance.cnpj_da_empresa:
-            self.fields['cnpj_da_empresa'].widget.attrs.update({'class': 'form-control form-control-md bg-light fs-6 text-dark'})
-            self.fields['cnpj_da_empresa'].label = "CNPJ da Empresa"
-
-        self.fields['email_do_representante_legal'].widget.attrs.update({'class': 'form-control form-control-md bg-light fs-6 text-dark'})
+        self.fields['email_do_representante_legal'].widget.attrs.update({
+            'class': 'form-control form-control-md fs-6 text-dark bg-light',
+        })
         self.fields['email_do_representante_legal'].label = "E-mail Administrativo"
